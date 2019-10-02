bfe7a4f6ab39791e1efba298cd2c09020a426ad1
Vladimir Dvorak <lada.dvorak7@gmail.com>
2019-10-02 19:09:39 +0200

    Incremental skyquality location import

--- imports/import_skyquality.py
+++ imports/import_skyquality.py
@@ -11,14 +11,15 @@ from lat_lon_parser import parse as lonlat_parse
 
 url = 'http://www.skyquality.cz/location/show?locationId='
 
+SKYQ_ORIGIN_ID_PREFIX = 'skyq_'
 
 class SkyQualityLocation(object):
     def __init__(self, **kwargs):
-        valid_keys = ["name", "create_by", "coords", "elevation", "descr", "accessibility", "sqm_avg", "bortle_scale"]
+        valid_keys = ["location_id", "name", "create_by", "coords", "elevation", "descr", "accessibility", "sqm_avg", "bortle_scale"]
         for key in valid_keys:
             setattr(self, key, kwargs.get(key))
 
-def scrapLocation(skyquality_location_id):
+def scrap_sq_location(skyquality_location_id):
         r = requests.get(url + str(skyquality_location_id))
 
         soup = BeautifulSoup(r.content, 'html.parser')
@@ -50,26 +51,80 @@ def scrapLocation(skyquality_location_id):
         if descr == 'NULL':
             descr = ''
 
-        location = SkyQualityLocation(name = name,
-                                      coords = coords,
-                                      create_by = soup.find(text='Vytvořil/a').find_next().text.strip(),
-                                      elevation = soup.find(text='Nadm. výška').find_next().text.strip(),
-                                      descr = descr,
-                                      accessibility = soup.find(text='Přístupnost').find_next().text.strip(),
-                                      sqm_avg = sqm_avg,
-                                      bortle_scale = bortle_scale,
+        sq_location = SkyQualityLocation(location_id = skyquality_location_id,
+                                        name = name,
+                                        coords = coords,
+                                        create_by = soup.find(text='Vytvořil/a').find_next().text.strip(),
+                                        elevation = soup.find(text='Nadm. výška').find_next().text.strip(),
+                                        descr = descr,
+                                        accessibility = soup.find(text='Přístupnost').find_next().text.strip(),
+                                        sqm_avg = sqm_avg,
+                                        bortle_scale = bortle_scale,
                                       )
+        print('Name:' + sq_location.name)
+        print('Created by:' + sq_location.create_by)
+        print('Coordinaktions:' + sq_location.coords)
+        print('Elevation:' + sq_location.elevation)
+        print('Descr:' + sq_location.descr)
+        print('Accessibility:' + sq_location.accessibility)
+        print('SQM avg:' + sq_location.sqm_avg)
+        print('Bortle:' + sq_location.bortle_scale)
 
-        print('Name:' + location.name)
-        print('Created by:' + location.create_by)
-        print('Coordinaktions:' + location.coords)
-        print('Elevation:' + location.elevation)
-        print('Descr:' + location.descr)
-        print('Accessibility:' + location.accessibility)
-        print('SQM avg:' + location.sqm_avg)
-        print('Bortle:' + location.bortle_scale)
+        return sq_location
 
-        return location
+def convert_sqlocation2location(sq_location, user_skyquality):
+        try:
+            bortle = float(sq_location.bortle_scale)
+            rating = round(10 - (bortle - 1))
+        except ValueError:
+            bortle = None
+            rating = 5
+
+        longitude = None
+        latitude = None
+
+        if sq_location.coords:
+            longLat = sq_location.coords.split(',')
+            if longLat and len(longLat) == 2:
+                try:
+                    longitude = lonlat_parse(longLat[0])
+                    latitude = lonlat_parse(longLat[1])
+                except ValueError:
+                    print('Unknown long-lat format=' + sq_location.coords)
+                    return None
+
+        is_for_observation = not any(x in sq_location.accessibility for x in ['nevhodné', 'nepřístupné'])
+
+        loc = Location(
+            name = sq_location.name,
+            longitude = longitude,
+            latitude = latitude,
+            descr = sq_location.descr,
+            bortle = bortle,
+            rating = rating,
+            #  sql_readings = db.relationship('SqlReading', backref='location', lazy=True)
+            country_code = 'CZ',
+            user_id = None,
+            is_public = True,
+            is_for_observation = is_for_observation,
+            origin_id = SKYQ_ORIGIN_ID_PREFIX + str(sq_location.location_id),
+            create_by = user_skyquality.id,
+            update_by = user_skyquality.id,
+            create_date = datetime.now(),
+            update_date = datetime.now(),
+        )
+
+        return loc
+
+
+def save_location(loc):
+    if loc:
+        db.session.add(loc)
+        try:
+            db.session.commit()
+        except IntegrityError as e:
+            print('Commit failed:', e)
+            db.session.rollback()
 
 
 def do_import_skyquality_locations(skyquality_db_name, delete_old):
@@ -89,82 +144,58 @@ def do_import_skyquality_locations(skyquality_db_name, delete_old):
 
     cur = db_connection.cursor()
     if delete_old:
-        cur.execute('delete from locations;')
+        cur.execute('delete from locations')
         db_connection.commit()
-        Location.query.filter_by(create_by=user_skyquality.id).delete()
-
-    sqlqry = "SELECT max(location_id) FROM locations;"
-    c = cur.execute(sqlqry)
-    c = c.fetchone();
-    skyquality_location_id = c[0] + 1 if c[0] else 1
-
-    while True:
-        skyQualityLocation = scrapLocation(skyquality_location_id)
-
-        if skyQualityLocation is None:
-            print('SkyQuality location_id=' + str(skyquality_location_id) + ' does not exist.')
-            print('Import finished.')
-            return
-
-        try:
-            bortle = float(skyQualityLocation.bortle_scale)
-            rating = round(10 - (bortle - 1))
-        except ValueError:
-            bortle = None
-            rating = 5
-
-        longitude = None
-        latitude = None
-
-        if skyQualityLocation.coords:
-            longLat = skyQualityLocation.coords.split(',')
-            if longLat and len(longLat) == 2:
-                try:
-                    longitude = lonlat_parse(longLat[0])
-                    latitude = lonlat_parse(longLat[1])
-                except ValueError:
-                    print('Unknown long-lat format=' + skyQualityLocation.coords)
-
-        is_for_observation = not any(x in skyQualityLocation.accessibility for x in ['nevhodné', 'nepřístupné'])
-
-        loc = Location(
-            name = skyQualityLocation.name,
-            longitude = longitude,
-            latitude = latitude,
-            descr = skyQualityLocation.descr,
-            bortle = bortle,
-            rating = rating,
-            #  sql_readings = db.relationship('SqlReading', backref='location', lazy=True)
-            country_code = 'CZ',
-            user_id = None,
-            is_public = True,
-            is_for_observation = is_for_observation,
-            origin_id = 'skyq_' + str(skyquality_location_id),
-            create_by = user_skyquality.id,
-            update_by = user_skyquality.id,
-            create_date = datetime.now(),
-            update_date = datetime.now(),
-        )
-
-        db.session.add(loc)
+        Location.query.filter(Location.origin_id.like(SKYQ_ORIGIN_ID_PREFIX +'%')).delete(synchronize_session=False)
         try:
             db.session.commit()
         except IntegrityError as e:
             print('Commit failed:', e)
             db.session.rollback()
+    else:
+        for row_sqloc in cur.execute("SELECT location_id,name,coords,create_by,elevation,descr,accessibility,sqm_avg,bortle_scale FROM locations").fetchall():
+            sq_location = SkyQualityLocation(location_id = row_sqloc[0],
+                                            name = row_sqloc[1],
+                                            coords = row_sqloc[2],
+                                            create_by = row_sqloc[3],
+                                            elevation = row_sqloc[4],
+                                            descr = row_sqloc[5],
+                                            accessibility = row_sqloc[6],
+                                            sqm_avg = row_sqloc[7],
+                                            bortle_scale = row_sqloc[8],
+                                            )
+            location = Location.query.filter_by(id=sq_location.location_id)
+            if not location:
+                loc = convert_sqlocation2location(sq_location, user_skyquality)
+                save_location(loc)
 
-        cur.execute("INSERT INTO locations (location_id, name, coords, elevation, descr, accessibility, sqm_avg, bortle_scale) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (
-            skyquality_location_id,
-            skyQualityLocation.name,
-            skyQualityLocation.coords,
-            skyQualityLocation.elevation,
-            skyQualityLocation.descr,
-            skyQualityLocation.accessibility,
-            skyQualityLocation.sqm_avg,
-            skyQualityLocation.bortle_scale
+    max_loc = cur.execute("SELECT max(location_id) FROM locations").fetchone()
+    skyquality_location_id = max_loc[0] + 1 if max_loc[0] else 1
+
+    while True:
+        sq_location = scrap_sq_location(skyquality_location_id)
+
+        if sq_location is None:
+            print('SkyQuality location_id=' + str(skyquality_location_id) + ' does not exist.')
+            print('Import finished.')
+            return
+
+        loc = convert_sqlocation2location(sq_location, user_skyquality)
+
+        save_location(loc)
+
+        cur.execute("INSERT INTO locations (location_id, name, coords, create_by, elevation, descr, accessibility, sqm_avg, bortle_scale) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (
+            sq_location.location_id,
+            sq_location.name,
+            sq_location.coords,
+            sq_location.create_by,
+            sq_location.elevation,
+            sq_location.descr,
+            sq_location.accessibility,
+            sq_location.sqm_avg,
+            sq_location.bortle_scale
             ))
 
         db_connection.commit()
 
         skyquality_location_id += 1
-
--- sql/skyquality.sql
+++ sql/skyquality.sql
@@ -1,8 +1,12 @@
 CREATE TABLE locations(location_id integer primary key,
     name varchar2(256),
     coords varchar2(128),
+    create_by varchar2(128),
     elevation varchar2(128),
     descr text,
     accessibility text,
     sqm_avg varchar2(32),
-    bortle_scale varchar2(32));
+    bortle_scale varchar2(32)
+    );
+
+ CREATE INDEX ix_locations_orig_id ON locations (orig_id);
