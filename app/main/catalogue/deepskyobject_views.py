2d6d36435e2f839ce5e64a45dc7c16069ad4b0b4
Vladimir Dvorak <lada.dvorak7@gmail.com>
2019-12-23 16:46:45 +0100

    Fchart updates

--- app/main/catalogue/deepskyobject_views.py
+++ app/main/catalogue/deepskyobject_views.py
@@ -109,7 +109,7 @@ def deepskyobject_findchart(dso_id):
     dso_dname = dso.denormalized_name().replace(' ','')
     pdf_file =  preview_dir + dso_dname + '.pdf'
     if not os.path.exists(pdf_file):
-        p = subprocess.Popen(['fchart', '-O', 'cairo', '-o', preview_dir, dso_dname, '-r', '-n'])
+        p = subprocess.Popen(['fchart', '-f', '5', '-o', preview_dir, dso_dname, '-r', '-n', '-c', ''])
         p.wait()
     fchart = preview_url_dir + dso_dname + '.pdf'
     return render_template('main/catalogue/deepskyobject_info.html', type='fchart', dso=dso, fchart=fchart,
