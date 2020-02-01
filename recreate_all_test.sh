a9e139540db21ac7b9ce56b901c693deca81b8a7
Vladimir Dvorak <lada.dvorak7@gmail.com>
2020-02-01 10:16:11 +0100

    recreate

--- recreate_all_test.sh
+++ recreate_all_test.sh
@@ -5,7 +5,7 @@ confirm() {
     echo "This operation recreate DB and initializes catalogues from OpenNGC data. It could be long."
     read -r -p "${1:-Are you sure to recreate all data in DB? [y/N]} " response
     case "$response" in
-        [yY][eE][sS]|[yY])
+        [yY][eE][sS]|[yY]) 
             true
             ;;
         *)
@@ -17,6 +17,8 @@ confirm() {
 ! confirm && exit;
 
 ./manage.py recreate_db
-./manage.py setup_prod
+./manage.py setup_dev
+./manage.py add_test_user
+./manage.py add_editor_user
 ./manage.py initialize_catalogues
 ./manage.py add_help_users
