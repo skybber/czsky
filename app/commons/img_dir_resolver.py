23cf1d793b5e0a8cde6dea701f63c2771df0162e
Vladimir Dvorak <lada.dvorak7@gmail.com>
2020-02-23 22:33:04 +0100

    Add forgotten file

---
+++ app/commons/img_dir_resolver.py
@@ -0,0 +1,24 @@
+import os
+
+img_paths = None
+
+def _load_img_ext_config():
+    global img_paths
+    if img_paths is None:
+        str_img_ext_count = os.environ.get('IMG_PATHS_COUNT')
+        if not str_img_ext_count or not str_img_ext_count.isdigit():
+            img_paths = []
+        else:
+            img_paths = []
+            for i in range(int(str_img_ext_count)):
+                img_paths.append((os.environ.get('IMG_PATHS_' + str(i+1) + '_DIR'), os.environ.get('IMG_PATHS_' + str(i+1) + '_REF')))
+    return img_paths
+
+def resolve_img_path_dir(img_file_name):
+    """Search img_file_name in all configured image paths"""
+    img_paths = _load_img_ext_config()
+    for dirdef in img_paths:
+        path = 'app' + os.path.join(dirdef[0], img_file_name)
+        if os.path.exists(path):
+            return dirdef
+    return '', ''
