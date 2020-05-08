8b0dd22c77c1940b35585d42cc0a8a1dbf744c45
Vladimir Dvorak <lada.dvorak7@gmail.com>
2020-05-08 10:53:02 +0200

    Fix assets

--- app/assets.py
+++ app/assets.py
@@ -8,16 +8,10 @@ dark_theme_css = Bundle('app.scss', filters='scss', output='styles/dark_theme.cs
 
 app_js = Bundle('app.js', filters='jsmin', output='scripts/app.js')
 
-vendor_css = Bundle('vendor/semantic.min.css',
-                    'vendor/easymde.min.css',
-                    'vendor/jquery-ui.min.css',
-                    'vendor/jquery-ui.structure.min.css',
-                    'vendor/jquery-ui.theme.min.css',
-                    output='styles/vendor.css')
+vendor_css = Bundle('vendor/semantic.min.css', 'vendor/easymde.min.css', output='styles/vendor.css')
 
 vendor_js = Bundle(
     'vendor/jquery.min.js',
-    'vendor/jquery-ui.min.js',
     'vendor/semantic.min.js',
     'vendor/tablesort.min.js',
     'vendor/easymde.min.js',
