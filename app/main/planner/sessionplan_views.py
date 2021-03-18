51714b120655bf8d098a2d774d6a39b015f2ecbb
Vladimir Dvorak <lada.dvorak7@gmail.com>
2021-03-18 21:10:36 +0100

    Planner dev

--- app/main/planner/sessionplan_views.py
+++ app/main/planner/sessionplan_views.py
@@ -31,6 +31,7 @@ from app.models import (
     Constellation,
     DeepskyObject,
     DsoList,
+    DsoListItem,
     Location,
     SessionPlan,
     SkyList,
@@ -106,143 +107,6 @@ def session_plan_info(session_plan_id):
         location = Location.query.filter_by(id=form.location_id.data).first()
 
     return render_template('main/planner/session_plan.html', tab='info', session_plan=session_plan, form=form, location=location)
-
-
-@main_sessionplan.route('/session-plan/<int:session_plan_id>/schedule', methods=['GET', 'POST'])
-@login_required
-def session_plan_schedule(session_plan_id):
-    """View a session plan schedule."""
-    session_plan = SessionPlan.query.filter_by(id=session_plan_id).first()
-    if session_plan is None:
-        abort(404)
-    if session_plan.user_id != current_user.id:
-        abort(404)
-
-    search_form = SessionPlanScheduleSearch()
-
-    sort_by = request.args.get('sortby')
-
-    ret, page = process_paginated_session_search('planner_search_page', [
-        ('planner_dso_type', search_form.dso_type),
-        ('planner_dso_obj_source', search_form.obj_source),
-        ('planner_dso_maglim', search_form.maglim),
-        ]);
-
-    if not ret:
-        return redirect(url_for('main_sessionplan.session_plan_schedule', session_plan_id=session_plan_id, page=page, sortby=sort_by))
-
-    per_page = get_items_per_page(search_form.items_per_page)
-
-    offset = (page - 1) * per_page
-
-    mag_scale = (8, 16)
-
-    sort_def = { 'name': DeepskyObject.name,
-                 'type': DeepskyObject.type,
-                 'constellation': DeepskyObject.constellation_id,
-                 'mag': DeepskyObject.mag,
-    }
-
-    table_sort = create_table_sort(sort_by, sort_def.keys())
-
-    if search_form.obj_source.data is None or search_form.obj_source.data == 'WL':
-        selection_list = []
-        wish_list = WishList.query.filter_by(user_id=current_user.id).first()
-        if wish_list and wish_list.wish_list_items:
-            for item in wish_list.wish_list_items:
-                if item.deepskyObject is not None:
-                    selection_list.append(item.deepskyObject)
-        all_count = len(selection_list)
-    elif search_form.obj_source.data.startswith('DL_'):
-        dso_list_id = int(search_form.obj_source.data[3:])
-        dso_list = DsoList.query.filter_by(id=dso_list_id).first()
-        selection_list = []
-        if dso_list:
-            selection_list = [ x.deepskyObject for x in dso_list.dso_list_items ]
-        else:
-            selection_list = []
-        all_count = len(selection_list)
-    else:
-        selection_dsos = DeepskyObject.query
-        cat_id = Catalogue.get_catalogue_id_by_cat_code(search_form.obj_source.data)
-        if cat_id:
-            selection_dsos = selection_dsos.filter_by(catalogue_id=cat_id)
-
-        if search_form.dso_type.data and search_form.dso_type.data != 'All':
-            selection_dsos = selection_dsos.filter(DeepskyObject.type==search_form.dso_type.data)
-
-        if search_form.maglim.data is not None and search_form.maglim.data < mag_scale[1]:
-            selection_dsos = selection_dsos.filter(DeepskyObject.mag<search_form.maglim.data)
-
-        order_by_field = None
-        if sort_by:
-            desc = sort_by[0] == '-'
-            sort_by_name = sort_by[1:] if desc else sort_by
-            order_by_field = sort_def.get(sort_by_name)
-            if order_by_field and desc:
-                order_by_field = order_by_field.desc()
-
-        if order_by_field is None:
-            order_by_field = DeepskyObject.id
-
-        page_dsos = selection_dsos.order_by(order_by_field).limit(per_page).offset(offset).all()
-
-        all_count = selection_dsos.count()
-
-        selection_list = page_dsos.copy()
-
-    pagination = Pagination(page=page, total=all_count, search=False, record_name='deepskyobjects', css_framework='semantic', not_passed_args='back')
-
-    for item in session_plan.sky_list.sky_list_items:
-        if item.deepskyObject:
-            selection_list = list(filter(lambda x: x.id != item.deepskyObject.id, selection_list))
-
-    add_form = AddToSessionPlanForm()
-    add_form.session_plan_id.data = session_plan.id
-    catalogues_menu_items = get_catalogues_menu_items()
-
-    dso_lists = DsoList.query.all()
-
-    loc = session_plan.location
-
-    loc_coords = EarthLocation.from_geodetic(loc.longitude*u.deg, loc.latitude*u.deg, loc.elevation*u.m if loc.elevation else 0)
-    t = Time(session_plan.for_date)
-    observer = Observer(name=loc.name, location=loc_coords, timezone='Europe/Prague')
-
-    selection_rms_list = rise_merid_set_time(t, observer, [ (x.ra, x.dec) for x in selection_list])
-    selection_compound_list = [ (selection_list[i], *selection_rms_list[i]) for i in range(len(selection_list))]
-
-    sli = session_plan.sky_list.sky_list_items
-    session_plan_rms_list = rise_merid_set_time(t, observer, [ (x.deepskyObject.ra, x.deepskyObject.dec) for x in sli])
-    session_plan_compound_list = [ (sli[i], *session_plan_rms_list[i]) for i in range(len(sli))]
-
-    return render_template('main/planner/session_plan.html', tab='schedule', session_plan=session_plan,
-                           selection_compound_list=selection_compound_list, session_plan_compound_list=session_plan_compound_list,
-                           dso_lists=dso_lists, catalogues_menu_items=catalogues_menu_items, mag_scale=mag_scale,
-                           add_form=add_form, search_form=search_form, pagination=pagination,table_sort=table_sort)
-
-
-def rise_merid_set_time(t, observer, ra_dec_list):
-    coords = [ SkyCoord(x[0] * u.rad, x[1] * u.rad) for x in ra_dec_list]
-    rise_list = to_HM_format(observer.target_rise_time(t, coords)) if len(coords) > 0 else []
-    merid_list = to_HM_format(observer.target_meridian_transit_time(t, coords))  if len(coords) > 0 else []
-    set_list = to_HM_format(observer.target_set_time(t, coords)) if len(coords) > 0 else []
-
-    return [(rise_list[i], merid_list[i], set_list[i]) for i in range(len(rise_list))]
-
-def to_HM_format(tm):
-    ret = []
-    try:
-        it = iter(tm)
-    except TypeError:
-        tm = [tm]
-
-    for t in tm:
-        try:
-            ret.append(t.strftime("%H:%M"))
-        except ValueError:
-            ret.append('')
-    return ret
 
 
 @main_sessionplan.route('/new-session-plan', methods=['GET', 'POST'])
@@ -419,3 +283,143 @@ def session_plan_upload(session_plan_id):
 
     session['is_backr'] = True
     return redirect(url_for('main_sessionplan.session_plan_schedule', session_plan_id=session_plan.id))
+
+@main_sessionplan.route('/session-plan/<int:session_plan_id>/schedule', methods=['GET', 'POST'])
+@login_required
+def session_plan_schedule(session_plan_id):
+    """View a session plan schedule."""
+    session_plan = SessionPlan.query.filter_by(id=session_plan_id).first()
+    if session_plan is None:
+        abort(404)
+    if session_plan.user_id != current_user.id:
+        abort(404)
+
+    search_form = SessionPlanScheduleSearch()
+
+    sort_by = request.args.get('sortby')
+
+    ret, page = process_paginated_session_search('planner_search_page', [
+        ('planner_dso_type', search_form.dso_type),
+        ('planner_dso_obj_source', search_form.obj_source),
+        ('planner_dso_maglim', search_form.maglim),
+        ]);
+
+    if not ret:
+        return redirect(url_for('main_sessionplan.session_plan_schedule', session_plan_id=session_plan_id, page=page, sortby=sort_by))
+
+    per_page = get_items_per_page(search_form.items_per_page)
+
+    offset = (page - 1) * per_page
+
+    mag_scale = (8, 16)
+
+    sort_def = { 'name': DeepskyObject.name,
+                 'type': DeepskyObject.type,
+                 'constellation': DeepskyObject.constellation_id,
+                 'mag': DeepskyObject.mag,
+    }
+
+    table_sort = create_table_sort(sort_by, sort_def.keys())
+
+    if search_form.obj_source.data is None or search_form.obj_source.data == 'WL':
+        subquery1 = db.session.query(WishListItem.dso_id) \
+            .join(WishListItem.wish_list) \
+            .filter(WishList.user_id==current_user.id)
+
+        dso_query = DeepskyObject.query \
+            .filter(DeepskyObject.id.in_(subquery1))
+
+    elif search_form.obj_source.data.startswith('DL_'):
+        dso_list_id = int(search_form.obj_source.data[3:])
+
+        subquery1 = db.session.query(DsoListItem.dso_id) \
+            .join(DsoListItem.dso_list) \
+            .filter(DsoList.id==dso_list_id)
+
+        dso_query = DeepskyObject.query \
+            .filter(DeepskyObject.id.in_(subquery1))
+    else:
+        dso_query = DeepskyObject.query
+        cat_id = Catalogue.get_catalogue_id_by_cat_code(search_form.obj_source.data)
+        if cat_id:
+            dso_query = dso_query.filter_by(catalogue_id=cat_id)
+
+        if search_form.dso_type.data and search_form.dso_type.data != 'All':
+            dso_query = dso_query.filter(DeepskyObject.type==search_form.dso_type.data)
+
+        if search_form.maglim.data is not None and search_form.maglim.data < mag_scale[1]:
+            dso_query = dso_query.filter(DeepskyObject.mag<search_form.maglim.data)
+
+    subquery = db.session.query(SkyListItem.dso_id) \
+        .join(SkyListItem.sky_list) \
+        .filter(SkyList.id==session_plan.sky_list_id)
+
+    dso_query = dso_query.filter(DeepskyObject.id.notin_(subquery))
+
+    order_by_field = None
+    if sort_by:
+        desc = sort_by[0] == '-'
+        sort_by_name = sort_by[1:] if desc else sort_by
+        order_by_field = sort_def.get(sort_by_name)
+        if order_by_field and desc:
+            order_by_field = order_by_field.desc()
+
+    if order_by_field is None:
+        order_by_field = DeepskyObject.id
+
+    page_dsos = dso_query.order_by(order_by_field).limit(per_page).offset(offset).all()
+
+    all_count = dso_query.count()
+
+    selection_list = page_dsos.copy()
+
+    pagination = Pagination(page=page, total=all_count, search=False, record_name='deepskyobjects', css_framework='semantic', not_passed_args='back')
+
+    add_form = AddToSessionPlanForm()
+    add_form.session_plan_id.data = session_plan.id
+    catalogues_menu_items = get_catalogues_menu_items()
+
+    dso_lists = DsoList.query.all()
+
+    loc = session_plan.location
+
+    loc_coords = EarthLocation.from_geodetic(loc.longitude*u.deg, loc.latitude*u.deg, loc.elevation*u.m if loc.elevation else 0)
+    t = Time(session_plan.for_date)
+    observer = Observer(name=loc.name, location=loc_coords, timezone='Europe/Prague')
+
+    selection_rms_list = rise_merid_set_time(t, observer, [ (x.ra, x.dec) for x in selection_list])
+    selection_compound_list = [ (selection_list[i], *selection_rms_list[i]) for i in range(len(selection_list))]
+
+    sli = session_plan.sky_list.sky_list_items
+    session_plan_rms_list = rise_merid_set_time(t, observer, [ (x.deepskyObject.ra, x.deepskyObject.dec) for x in sli])
+    session_plan_compound_list = [ (sli[i], *session_plan_rms_list[i]) for i in range(len(sli))]
+
+    return render_template('main/planner/session_plan.html', tab='schedule', session_plan=session_plan,
+                           selection_compound_list=selection_compound_list, session_plan_compound_list=session_plan_compound_list,
+                           dso_lists=dso_lists, catalogues_menu_items=catalogues_menu_items, mag_scale=mag_scale,
+                           add_form=add_form, search_form=search_form, pagination=pagination,table_sort=table_sort)
+
+
+def rise_merid_set_time(t, observer, ra_dec_list):
+    coords = [ SkyCoord(x[0] * u.rad, x[1] * u.rad) for x in ra_dec_list]
+    rise_list = to_HM_format(observer.target_rise_time(t, coords)) if len(coords) > 0 else []
+    merid_list = to_HM_format(observer.target_meridian_transit_time(t, coords))  if len(coords) > 0 else []
+    set_list = to_HM_format(observer.target_set_time(t, coords)) if len(coords) > 0 else []
+
+    return [(rise_list[i], merid_list[i], set_list[i]) for i in range(len(rise_list))]
+
+def to_HM_format(tm):
+    ret = []
+    try:
+        it = iter(tm)
+    except TypeError:
+        tm = [tm]
+
+    for t in tm:
+        try:
+            ret.append(t.strftime("%H:%M"))
+        except ValueError:
+            ret.append('')
+    return ret
+
+
