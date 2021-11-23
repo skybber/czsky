90bca1bc723c38d3246b4c69a6d4b7d3d62d0e65
Vladimir Dvorak <lada.dvorak7@gmail.com>
2021-11-23 19:42:18 +0100

    Prettify sql remove

--- app/main/observation/observation_form_utils.py
+++ app/main/observation/observation_form_utils.py
@@ -93,8 +93,7 @@ def update_from_basic_form(form, observation):
 
     if observation.id is not None:
         for item in observation.observation_items:
-            for dso in item.deepsky_objects:
-                item.deepsky_objects.remove(dso)
+            item.deepsky_objects = []
             db.session.delete(item)
         observation.observation_items.clear()
 
