321305339d7ebb1ff5def94bde2bca6c994f55e2
Vladimir Dvorak <lada.dvorak7@gmail.com>
2022-04-06 20:10:35 +0200

    Fix user_id

--- app/main/observation/observing_session_views.py
+++ app/main/observation/observing_session_views.py
@@ -234,7 +234,7 @@ def observing_session_items_edit(observing_session_id):
                 dsos, notes = _parse_compound_notes(item_form.comp_notes.data)
                 observation = Observation(
                     observing_session_id=observing_session.id,
-                    user=current_user.id,
+                    user_id=current_user.id,
                     date_from=item_time,
                     date_to=item_time,
                     notes=notes,
