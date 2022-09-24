833df2fbc156dc941cb16bbfac8f9381a0e4e773
Vladimir Dvorak <lada.dvorak7@gmail.com>
2022-09-24 08:42:17 +0200

    Dbl star list improvements

--- app/main/catalogue/double_star_list_forms.py
+++ app/main/catalogue/double_star_list_forms.py
@@ -24,12 +24,14 @@ from wtforms.validators import (
     InputRequired,
     Length,
     NumberRange,
+    Optional,
     required
 )
 from flask_babel import lazy_gettext
 
 
 class SearchDoubleStarListForm(FlaskForm):
+    q = StringField('Search')
     season = SelectField(lazy_gettext('Season'), choices=[
         ('All', lazy_gettext('All')),
         ('winter', lazy_gettext('Winter')),
@@ -38,4 +40,9 @@ class SearchDoubleStarListForm(FlaskForm):
         ('autumn', lazy_gettext('Autumn')),
         ('southern', lazy_gettext('Southern')),
     ], default='')
+    mag_max = FloatField(lazy_gettext('Mag max'), default=12.0)
+    delta_mag_min = FloatField(lazy_gettext('Delta mag min'), default=0.0)
+    separation_min = FloatField(lazy_gettext('Separation min'), default=1.0)
+    separation_max = FloatField(lazy_gettext('Separation max'), default=60.0)
+    dec_min = FloatField(lazy_gettext('Dec min'), default=-35.0)
 
--- app/main/catalogue/double_star_list_views.py
+++ app/main/catalogue/double_star_list_views.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 import base64
 
+import numpy as np
+from sqlalchemy import or_
+
 from flask import (
     abort,
     Blueprint,
@@ -17,7 +20,16 @@ from flask_login import current_user, login_required
 from app import db
 from posix import wait
 
-from app.models import Constellation, DoubleStarList, DoubleStarListItem, DoubleStarListDescription, User, UserDsoDescription
+from app.models import (
+    Constellation,
+    DoubleStar,
+    DoubleStarList,
+    DoubleStarListItem,
+    DoubleStarListDescription,
+    User,
+    UserDsoDescription,
+)
+
 from app.commons.dso_utils import CHART_DOUBLE_STAR_PREFIX
 from app.commons.search_utils import process_session_search
 from app.commons.utils import get_lang_and_editor_user_from_request
@@ -34,6 +46,13 @@ from .double_star_list_forms import (
 )
 
 from app.main.chart.chart_forms import ChartForm
+from app.commons.search_utils import (
+    process_paginated_session_search,
+    get_items_per_page,
+    create_table_sort,
+    get_packed_constell_list,
+)
+
 
 main_double_star_list = Blueprint('main_double_star_list', __name__)
 
@@ -62,21 +81,76 @@ def double_star_list_info(double_star_list_id):
         abort(404)
 
     search_form = SearchDoubleStarListForm()
+
     if search_form.season.data == 'All':
         search_form.season.data = None
 
-    if not process_session_search([('double_star_list_season', search_form.season),]):
-        return redirect(url_for('main_double_star_list.double_star_list_info', double_star_list_id=double_star_list_id, season=search_form.season.data))
+    ret, page, sort_by = process_paginated_session_search('dbl_star_list_search_page', 'dbl_list_sort_by', [
+        ('dbl_star_search', search_form.q),
+        ('dbl_list_season', search_form.season),
+        ('dbl_list_mag_max', search_form.mag_max),
+        ('dbl_list_delta_mag_min', search_form.delta_mag_min),
+        ('dbl_list_separation_min', search_form.separation_min),
+        ('dbl_list_separation_max', search_form.separation_max),
+        ('dec_list_min', search_form.dec_min),
+    ])
 
-    season = request.args.get('season', None)
-    constell_ids = Constellation.get_season_constell_ids(season)
+    if not ret:
+        return redirect(url_for('main_double_star_list.double_star_list_info', double_star_list_id=double_star_list_id, sortby=sort_by))
 
     lang, editor_user = get_lang_and_editor_user_from_request(for_constell_descr=True)
+
+    dbl_list_query = DoubleStarListItem.query.filter(DoubleStarListItem.double_star_list_id == double_star_list.id) \
+        .join(DoubleStarListItem.double_star, aliased=True)
+
+    if search_form.q.data:
+        dbl_list_query = dbl_list_query.filter(or_(DoubleStar.common_cat_id == search_form.q.data,
+                                                   DoubleStar.wds_number == search_form.q.data.strip(),
+                                                   DoubleStar.norm_other_designation.like('%;' + search_form.q.data.strip() + ';%')
+                                                   ))
+    else:
+        if search_form.mag_max.data is not None:
+            dbl_list_query = dbl_list_query.filter(DoubleStar.mag_first < search_form.mag_max.data, DoubleStar.mag_second < search_form.mag_max.data)
+        if search_form.delta_mag_min.data is not None:
+            dbl_list_query = dbl_list_query.filter(DoubleStar.delta_mag > search_form.delta_mag_min.data)
+        if search_form.separation_min.data is not None:
+            dbl_list_query = dbl_list_query.filter(DoubleStar.separation > search_form.separation_min.data)
+        if search_form.separation_max.data is not None:
+            dbl_list_query = dbl_list_query.filter(DoubleStar.separation < search_form.separation_max.data)
+        if search_form.dec_min.data is not None:
+            dbl_list_query = dbl_list_query.filter(DoubleStar.dec_first > (np.pi * search_form.dec_min.data / 180.0))
+
+    sort_def = { 'wds_number': DoubleStar.wds_number,
+                 'common_cat_id': DoubleStar.common_cat_id,
+                 'components': DoubleStar.components,
+                 'other_designation': DoubleStar.other_designation,
+                 'ra': DoubleStar.ra_first,
+                 'dec': DoubleStar.dec_first,
+                 'mag_first': DoubleStar.mag_first,
+                 'mag_second': DoubleStar.mag_second,
+                 'separation': DoubleStar.separation,
+                 'spectral_type': DoubleStar.spectral_type,
+                 'constellation': DoubleStar.constellation_id,
+                 }
+
+    table_sort = create_table_sort(sort_by, sort_def.keys())
+
+    order_by_field = None
+    if sort_by:
+        desc = sort_by[0] == '-'
+        sort_by_name = sort_by[1:] if desc else sort_by
+        order_by_field = sort_def.get(sort_by_name)
+        if order_by_field and desc:
+            order_by_field = order_by_field.desc()
+
+    sel_double_star_list_items = dbl_list_query.order_by(order_by_field).all()
+
+    constell_ids = Constellation.get_season_constell_ids(search_form.season.data)
 
     double_star_list_descr = DoubleStarListDescription.query.filter_by(double_star_list_id=double_star_list.id, lang_code=lang).first()
 
     double_star_list_items = []
-    for double_star_list_item in double_star_list.double_star_list_items:
+    for double_star_list_item in sel_double_star_list_items:
         if constell_ids is None or double_star_list_item.double_star.constellation_id in constell_ids:
             double_star_list_items.append(double_star_list_item)
 
@@ -84,7 +158,7 @@ def double_star_list_info(double_star_list_id):
     inverted_accordion = theme in ['dark', 'night']
 
     return render_template('main/catalogue/double_star_list_info.html', double_star_list=double_star_list, type='info', double_star_list_descr=double_star_list_descr, double_star_list_items=double_star_list_items,
-                           season=season, search_form=search_form, inverted_accordion=inverted_accordion)
+                           search_form=search_form, inverted_accordion=inverted_accordion, table_sort=table_sort)
 
 
 @main_double_star_list.route('/double-star-list/<string:double_star_list_id>/chart', methods=['GET', 'POST'])
