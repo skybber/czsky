978eb988011fca1fff4914bc3da45f477ea6599b
Vladimir Dvorak <lada.dvorak7@gmail.com>
2022-09-24 19:00:14 +0200

    ForceReload on update urls

--- app/static/js/fchart.js
+++ app/static/js/fchart.js
@@ -256,7 +256,7 @@ function FChart (fchartDiv, fldSizeIndex, fieldSizes, ra, dec, obj_ra, obj_dec, 
     window.addEventListener('resize', (function(e) {
         this.adjustCanvasSize();
         this.reloadLegendImage();
-        this.forceReload();
+        this.forceReloadImage();
     }).bind(this), false);
 
     $(this.canvas).bind('click', this.onClick.bind(this));
@@ -349,13 +349,13 @@ FChart.prototype.updateUrls = function(legendUrl, chartUrl) {
     this.legendUrl = legendUrl;
     this.chartUrl = chartUrl;
     this.reloadLegendImage();
-    this.forceReload();
+    this.forceReloadImage();
 };
 
 FChart.prototype.onWindowLoad = function() {
     this.adjustCanvasSize();
     this.reloadLegendImage();
-    this.forceReload();
+    this.forceReloadImage();
 }
 
 FChart.prototype.adjustCanvasSize = function() {
