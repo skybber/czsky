21570b1cf2d26bf76e7d95893fcba1ecad180262
Vladimir Dvorak <lada.dvorak7@gmail.com>
2023-11-10 21:02:47 +0100

    doublestar_info view in embed

--- app/main/catalogue/double_star_views.py
+++ app/main/catalogue/double_star_views.py
@@ -380,6 +380,9 @@ def double_star_seltab(double_star_id):
     if is_splitview_supported():
         return _do_redirect('main_double_star.double_star_chart', double_star, splitview=True)
 
+    if request.args.get('embed'):
+        return _do_redirect('main_double_star.double_star_info', double_star)
+
     return _do_redirect('main_double_star.double_star_chart', double_star)
 
 
