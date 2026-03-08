eddbd7c240600d5f3c79e2ae32880f191c5e5287
Vladimir Dvorak <lada.dvorak7@gmail.com>
2026-03-08 10:13:10 +0100

    Fix texture seam artifacts and update texture wrapping to allow repeating

--- app/static/js/sky_scene_planet_texture_renderer.js
+++ app/static/js/sky_scene_planet_texture_renderer.js
@@ -133,7 +133,8 @@
                 const z = Math.cos(phi) * sinT;
                 positions.push(x, y, z);
                 // Shift U by 0.5 so that the texture center (prime meridian) is at the front
-                const u = (1.5 - lon / lonSegs) % 1.0;
+                // Don't use modulo to avoid seam artifacts - let U go outside 0-1 range
+                const u = 1.5 - lon / lonSegs;
                 uvs.push(u, 1 - lat / latSegs);
             }
         }
@@ -230,7 +231,7 @@
         image.onload = function () {
             gl.bindTexture(gl.TEXTURE_2D, texture);
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
